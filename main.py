48d816bfffeaf7ccac83c625cc30e7d964d2d80e
rishab <rishabmallick20004@gmail.com>
2026-03-26 00:43:14 +0530

    ollama library

---
+++ main.py
@@ -0,0 +1,14 @@
+# Ollama via library
+# https://pypi.org/project/ollama/
+# import ollama
+# response = ollama.generate(model="ministral-3:3b", prompt="sir what is array" ,system="You are a cse professor")
+# print(response.response)
+
+# local_models = ollama.list()
+
+# progress = ollama.pull(model="ministral-3:3b" stream=True)
+
+# model_details = ollama.show("ministral-3:3b")
+# model_dict =model_details.dict()
+# print(model_dict["capabilities"])
+# print(model_dict["parameters"])
